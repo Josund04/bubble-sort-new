6c623f5a4af6e9441c92486bbf2652d7379b14c6
Josund04 <89245272+Josund04@users.noreply.github.com>
2022-02-18 13:25:24 +0100

    Add files via upload

---
+++ bubble sort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace bubble_sort
+{
+    class Program
+    {
+        
+      static void Main(string[] args) {
+    
+	
+		Random rnd = new Random();
+	int[] arr = new int[4000];
+    for (int j = 0; j < 4000; j++)
+		{
+			arr[j]=(rnd.Next(1,4000)); // returns random integers >= 10 and < 19
+		}
+	
+
+         Stopwatch stop=Stopwatch.StartNew();
+         stop.Start();
+         
+         int temp;
+         for (int j = 0; j <= arr.Length - 2; j++) {
+            for (int i = 0; i <= arr.Length - 2; i++) {
+               if (arr[i] > arr[i + 1]) {
+                  temp= arr[i + 1];
+                  arr[i + 1] = arr[i];
+                  arr[i] = temp;
+               }
+            }
+         }
+         stop.Stop(); 
+
+         Console.WriteLine("Sorted:");
+         foreach (int p in arr)
+            Console.Write(p + " ");
+            Console.WriteLine(" ");
+            Console.WriteLine("tid = " + stop.ElapsedMilliseconds + " ms"); 
+         Console.Read();
+      }
+   }
+}
